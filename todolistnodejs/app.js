cf9583110adc1e150d5a6e31fd5ca9bc210c205d
Heissendo <heissendo88@gmail.com>
2024-07-05 10:27:07 +0300

    typo fixes

--- todolistnodejs/app.js
+++ todolistnodejs/app.js
@@ -4,17 +4,12 @@ const path = require('path');
 const app = express();
 const port = 3000; 
 
-// Serve static files from the 'public' directory
+// Serve static files from the 'src' directory
 app.use(express.static(path.join(__dirname, 'src')));
 
 // Handle requests to the root URL
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
-
-// API endpoint to test server functionality
-app.get('/api/test', (req, res) => {
-    res.json({ message: 'Node.js server is running and API is working!' });
 });
 
 // Start the server
